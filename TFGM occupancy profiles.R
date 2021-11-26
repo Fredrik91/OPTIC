1e9593ed4c2263ed10a97639ca71461bbde241a4
Fredrik91 <92728083+Fredrik91@users.noreply.github.com>
2021-11-26 16:31:37 +0000

    Update TFGM occupancy profiles.R

--- TFGM occupancy profiles.R
+++ TFGM occupancy profiles.R
@@ -2,14 +2,19 @@
 ##
 #### TFGM occupancy data generation
 
-## Loading library
+### The aim is to generate occupancy profiles for each connector on the TFGM network, and subsequently compile this.
+### Compiling this yields occupancy profiles across the network (or selected subsets of the network). Statistics can also be derived
+### At the end, the whole timeseries can be aggregated to weekdays and weekends to get a general sense of charging profiles. 
+
+
+#### step 1. Loading library ####
 rm(list = ls())
 
 library(tidyverse)
 library(lubridate)
 library(splitstackshape)
 
-### step 1. Generate time series for 2018
+### step 2. Generate time series for 2018 ####
 
 start <- as.POSIXct("2018-01-01")
 interval <- 60
@@ -22,16 +27,16 @@ timestamps <- rename(timestamps, time_stamp = `seq(from = start, by = interval *
 
 
 
-### step 2. Load in TFGM dataset
+### step 3. Load in TFGM dataset ####
 df = read_csv("C:/Users/cvfm9/Loughborough University/Craig Morton - VPACH/Phase 2/Analysis/TripAttraction/Data/Greater Manchester EV data/TFGM cleaned data.csv")
 
 df <- tibble::rowid_to_column(df, "charge_id")
-### step 3. Generate ID by site, cpid, connector.In total 306 ID's are generated. 
+### step 4. Generate ID by site, cpid, connector.In total 306 ID's are generated. #### 
 ### for each connector, charge profiles should be generated and then linked back together to form time series plots on occupancy, or to enable occupancy calculations
 
 df<- df %>% group_by(Site,`CP ID`,Connector)%>% dplyr::mutate(ID=cur_group_id())
 
-df_1 <- subset(df, ID==1)
+df_1 <- subset(df, ID==111)
 df_1 <- select(df_1,c("Total kWh","start_time_stamp","end_time_stamp","charge_id"))
 #expand by 2 and then generate timestamp for start and end. This can subsequently be merged with timestamp dataframe
 df_1 <- expandRows(df_1, count = 2, count.is.col=FALSE)
@@ -50,9 +55,9 @@ df_1 <-df_1 %>%
   mutate(time_stamp = ymd_hms(time_stamp)) %>%
   complete(time_stamp = seq(floor_date(min(time_stamp),"minute"), max(time_stamp),60)) %>%
   fill(row_odd,  .direction = "up")
-#df_1$time_stamp <- round_date(df_1$time_stamp,unit="1 minute")
+
 df_1<-subset(df_1, row_odd==1)
-#df_1 <- unique(df_1)
+
 df_1 <-ungroup(df_1)
 df_1<-select(df_1,c("row_odd","time_stamp"))
 
