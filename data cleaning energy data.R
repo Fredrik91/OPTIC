799f55ad7471bd0eeb226a4d30def02c8fcbcde6
Fredrik91 <92728083+Fredrik91@users.noreply.github.com>
2022-02-15 17:03:57 +0000

    Update data cleaning energy data.R

--- data cleaning energy data.R
+++ data cleaning energy data.R
@@ -5,9 +5,9 @@ rm(list = ls())
 
 library(tidyverse)
 
-energy_18 <- c(9:12) %>% paste0("C:/Users/cvfm9/Loughborough University/Craig Morton - VPACH/Phase 2/Analysis/TripAttraction/Data/Greater Manchester EV data/Carbon intensity data/Carbon_Intensity_Data_2018_", .,".csv") %>% map(read_csv)
+energy_18 <- c(9:12) %>% paste0("C:/Users/cvfm9/OneDrive - Loughborough University/OPTIC/Carbon Intensity data/Carbon_Intensity_Data_2018_", .,".csv") %>% map(read_csv)
 energy_18 <-do.call(rbind.data.frame, energy_18)
-energy_19 <- c(1:2) %>% paste0("C:/Users/cvfm9/Loughborough University/Craig Morton - VPACH/Phase 2/Analysis/TripAttraction/Data/Greater Manchester EV data/Carbon intensity data/Carbon_Intensity_Data_2019_", .,".csv") %>% map(read_csv)
+energy_19 <- c(1:2) %>% paste0("C:/Users/cvfm9/OneDrive - Loughborough University/OPTIC/Carbon Intensity data/Carbon_Intensity_Data_2019_", .,".csv") %>% map(read_csv)
 energy_19 <-do.call(rbind.data.frame, energy_19)
 
 energy<- rbind(energy_18,energy_19)
@@ -28,7 +28,7 @@ ggplot(data=energy, aes(x=`Datetime (UTC)`,y=`North West England`)) + geom_line(
 
 
 ## write to csv
-write_csv(energy,"C:/Users/cvfm9/Loughborough University/Craig Morton - VPACH/Phase 2/Analysis/TripAttraction/Data/Greater Manchester EV data/carbon intensity data.csv")
+write_csv(energy,"C:/Users/cvfm9/OneDrive - Loughborough University/OPTIC/carbon intensity data.csv")
 
 
 
